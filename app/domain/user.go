6140b5440d9c1c568daac9c32e55d5c845839c10
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-05 18:51:55 +0900

    update: 外側の層からドメイン層の構造体を取得だけできるようにするため、ゲッタを定義

--- app/domain/user.go
+++ app/domain/user.go
@@ -18,3 +18,15 @@ func NewUser(id string, name string, password string) User {
 		password: password,
 	}
 }
+
+func (user User) ID() string {
+	return user.id
+}
+
+func (user User) Name() string {
+	return user.name
+}
+
+func (user User) Password() string {
+	return user.password
+}
