f3b1190dcdb6b724c21abcd7e2fca2f8308a26fe
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-05 18:57:04 +0900

    update: ユーザー名の重複を許さない処理を追加するため、コントローラー層でユーザー登録時にエラーの処理を追加

--- app/api/controller/create_user.go
+++ app/api/controller/create_user.go
@@ -16,3 +16,25 @@ func NewCreateUser(uc usecase.CreateUser) CreateUser {
 		uc: uc,
 	}
 }
+
+func (con CreateUser) Execute(ctx *gin.Context) {
+	var input usecase.CreateUserInput
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := con.uc.Execute(input)
+
+	if err != nil {
+		if err.Error() == "TODO: 適切なエラー文" {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "This name is already registered"})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, user)
+}
