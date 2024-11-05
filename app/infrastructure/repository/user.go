773c46e99fcdac5f8c70d6225a87a6481d7a13ea
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-05 19:00:41 +0900

    update: ユーザー登録と、重複するユーザー名がいるかをフェッチする処理の関数を追加

--- app/infrastructure/repository/user.go
+++ app/infrastructure/repository/user.go
@@ -17,3 +17,39 @@ func NewUserRepository(db *sql.DB) domain.UserRepository {
 	}
 }
 
+func (repo UserRepository) Create(user domain.User) (domain.User, error) {
+	query := `
+		INSERT INTO users (id, name, password) VALUES ($1, $2, $3);
+	`
+	_, err := repo.db.Exec(query, user.ID(), user.Name(), user.Password())
+
+	if err != nil {
+		return domain.User{}, err
+	}
+
+	return user, nil
+}
+
+func (repo UserRepository) FindByName(name string) (domain.User, error) {
+	query := `
+		SELECT id, name, password FROM users WHERE name=$1;
+	`
+
+	row := repo.db.QueryRow(query, name)
+
+	if err := row.Err(); err != nil {
+		return domain.User{}, errors.New("でーたべーすから値取れず")
+	}
+
+	var dbUser dbUser
+
+	err := row.Scan(&dbUser.id, &dbUser.name, &dbUser.password)
+
+	if err != nil {
+		return domain.User{}, errors.New("DBユーザー型に置き換えれず")
+	}
+
+	user := domain.NewUser(dbUser.id, dbUser.name, dbUser.password)
+
+	return user, nil
+}
