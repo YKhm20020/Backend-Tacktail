82b21342e57cd3aba01e52058d2d07e93c2a22de
KakinokiKanta <kanta12621@gmail.com>
2024-11-12 04:23:44 +0900

    add: データベース用の構造体をrepository/models.goに定義

--- app/infrastructure/repository/models.go
+++ app/infrastructure/repository/models.go
@@ -5,3 +5,36 @@ type dbUser struct {
 	name     string
 	password string
 }
+
+type dbCocktail struct {
+	id          string
+	name        string
+	description string
+}
+
+type dbRecipe struct {
+	id         string
+	cocktailID string
+	materialID string
+	amount     int
+}
+
+type dbImage struct {
+	id         string
+	cocktailID string
+	userID     string
+	image      string
+}
+
+type dbMaterial struct {
+	id          string
+	name        string
+	description string
+}
+
+type dbStory struct {
+	id         string
+	cocktailID string
+	trivia     string
+	day        int
+}
