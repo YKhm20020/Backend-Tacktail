49c327af1ae54a6f6370f15e3052ab499856262a
KakinokiKanta <kanta12621@gmail.com>
2024-11-11 17:28:25 +0900

    add: ログインユースケース用のDTOを定義

--- app/usecase/login.go
+++ app/usecase/login.go
@@ -8,6 +8,11 @@ type Login struct {
 	repo domain.UserRepository
 }
 
+type LoginInput struct {
+	Name     string `json:"name" binding:"required,min=1,max=20"`
+	Password string `json:"password" binding:"required,min=1"`
+}
+
 func NewLogin(repo domain.UserRepository) Login {
 	return Login{
 		repo: repo,
