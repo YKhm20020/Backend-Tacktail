ed9c3206304e5195a70a748b79ffee638c17446c
KakinokiKanta <kanta12621@gmail.com>
2024-11-12 04:36:37 +0900

    add: cocktailドメインで定義されたリポジトリーの関数をシグネチャだけ実装

--- app/infrastructure/repository/cocktail.go
+++ app/infrastructure/repository/cocktail.go
@@ -15,3 +15,22 @@ func NewCocktailRepository(db *sql.DB) domain.CocktailRepository {
 		db: db,
 	}
 }
+
+func (repo CocktailRepository) UpdateCocktailImage(
+	userID string,
+	cocktailID string,
+	image string,
+) error {
+	return nil
+}
+
+func (repo CocktailRepository) FindAll(userID string) ([]domain.Cocktail, error) {
+	return []domain.Cocktail{}, nil
+}
+
+func (repo CocktailRepository) FindByMaterials(
+	userID string,
+	materialIDs []string,
+) ([]domain.Cocktail, error) {
+	return []domain.Cocktail{}, nil
+}
