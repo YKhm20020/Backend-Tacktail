12f86e2b5879765584263eef338c2ebdb5ed01da
KakinokiKanta <kanta12621@gmail.com>
2024-11-12 16:43:09 +0900

    add: カクテル一覧取得用コントローラーを実装

--- app/api/controller/find_cocktail_list.go
+++ app/api/controller/find_cocktail_list.go
@@ -1,6 +1,11 @@
 package controller
 
-import "github.com/YKhm20020/Backend-Tacktail/usecase"
+import (
+	"net/http"
+
+	"github.com/YKhm20020/Backend-Tacktail/usecase"
+	"github.com/gin-gonic/gin"
+)
 
 type FindCocktailList struct {
 	uc usecase.FindCocktailList
@@ -10,4 +15,27 @@ func NewFindCocktailList(uc usecase.FindCocktailList) FindCocktailList {
 	return FindCocktailList{
 		uc: uc,
 	}
+}
+
+func (con FindCocktailList) Execute(ctx *gin.Context) {
+	var input usecase.FindCocktailListInput
+
+	// /cocktail/list?materials[]={material_id}?materials[]={material_id} でフィルターできるようにするため
+	input.MaterialIDs = ctx.QueryArray("materials[]")
+
+	// cookieからユーザーIDを取得
+	user_id, exists := ctx.Get("user_id")
+	if exists {
+		input.UserID = user_id.(string)
+	} else {
+		input.UserID = ""
+	}
+
+	output, err := con.uc.Execute(input)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, output)
 }
