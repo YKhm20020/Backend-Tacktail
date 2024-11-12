6bd060e3604715102821029295687dd65a7208c6
KakinokiKanta <kanta12621@gmail.com>
2024-11-12 22:04:01 +0900

    update: FindByMaterialsを修正

--- app/infrastructure/repository/cocktail.go
+++ app/infrastructure/repository/cocktail.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"database/sql"
-	"fmt"
 
 	"github.com/YKhm20020/Backend-Tacktail/domain"
 )
@@ -28,24 +27,24 @@ func (repo CocktailRepository) UpdateCocktailImage(
 func (repo CocktailRepository) FindAll(userID string) (map[string]domain.Cocktail, error) {
 	query := `
 		SELECT
-		cocktails.id, cocktails.name, cocktails.description, cocktail_images.image,
-			materials.id, materials.name, materials.description, recipes.amount
+			cocktails.id, cocktails.name, cocktails.description, cocktail_images.image,
+				materials.id, materials.name, materials.description, recipes.amount
 		FROM
-		cocktails
+			cocktails
 		LEFT JOIN
-		cocktail_images
+			cocktail_images
 		ON
-		cocktails.id = cocktail_images.cocktailID
+			cocktails.id = cocktail_images.cocktailID
 		AND
-		cocktail_images.userID = $1
+			cocktail_images.userID = $1
 		INNER JOIN
-		recipes
+			recipes
 		ON
-		cocktails.id = recipes.cocktailID
+			cocktails.id = recipes.cocktailID
 		INNER JOIN
-		materials
+			materials
 		ON
-		recipes.materialID = materials.id
+			recipes.materialID = materials.id
 	`
 
 	rows, err := repo.db.Query(query, userID)
@@ -82,11 +81,11 @@ func (repo CocktailRepository) FindAll(userID string) (map[string]domain.Cocktai
 		// マップへ紐づけ
 		materialMap[dbCocktail.id] = append(materialMap[dbCocktail.id], material)
 
-			// カクテルドメインのオブジェクトを生成
-			cocktail := domain.NewCocktail(dbCocktail.id, dbCocktail.name, dbCocktail.description, dbCocktailImage.image.String, materialMap[dbCocktail.id])
-			// マップへ紐づけ
-			cocktailMap[dbCocktail.id] = cocktail
-		}
+		// カクテルドメインのオブジェクトを生成
+		cocktail := domain.NewCocktail(dbCocktail.id, dbCocktail.name, dbCocktail.description, dbCocktailImage.image.String, materialMap[dbCocktail.id])
+		// マップへ紐づけ
+		cocktailMap[dbCocktail.id] = cocktail
+	}
 
 	return cocktailMap, nil
 }
@@ -94,46 +93,58 @@ func (repo CocktailRepository) FindAll(userID string) (map[string]domain.Cocktai
 func (repo CocktailRepository) FindByMaterials(
 	userID string,
 	materialIDs []string,
-) ([]domain.Cocktail, error) {
+) (map[string]domain.Cocktail, error) {
 	query := `
 		SELECT
-		cocktails.id, cocktails.name, cocktails.description, cocktail_images.image,
-			materials.id, materials.name, materials.description, recipes.amount
+			cocktails.id, cocktails.name, cocktails.description, cocktail_images.image,
+				materials.id, materials.name, materials.description, recipes.amount
 		FROM
-		cocktails
+			cocktails
 		LEFT JOIN
-		cocktail_images
+			cocktail_images
 		ON
-		cocktails.id = cocktail_images.cocktailID
+			cocktails.id = cocktail_images.cocktailID
 		AND
-		cocktail_images.userID = $1
+			cocktail_images.userID = ''
 		INNER JOIN
-		recipes
+			recipes
 		ON
-		cocktails.id = recipes.cocktailID
+			cocktails.id = recipes.cocktailID
 		INNER JOIN
-		materials
+			materials
 		ON
-		recipes.materialID = materials.id
+			recipes.materialID = materials.id
+		WHERE
+			cocktails.id 
+		IN
+			(
+				SELECT
+					recipes.cocktailID
+				FROM
+					recipes
+				WHERE
+					recipes.materialID
+				IN
+					('material01_id', 'material02_id')
+			)
 	`
 
 	rows, err := repo.db.Query(query, userID)
 	if err != nil {
-		return []domain.Cocktail{}, err
+		// return []domain.Cocktail{}, err
+		return nil, err
 	}
-
-	// カクテル一覧を格納する変数
-	var cocktails []domain.Cocktail
-	// カクテルに対する材料一覧を格納する変数
-	var materials []domain.Material
 
 	// クエリの実行結果をバインドするための変数
 	var dbCocktail dbCocktail
 	var dbMaterial dbMaterial
 	var dbCocktailImage dbCocktailImage
 	var dbRecipe dbRecipe
-	// カクテルに材料のリストを持たせるため、カクテルIDは別で宣言
-	cocktailID := ""
+
+	// カクテルIDからカクテル構造体へのマップ
+	cocktailMap := make(map[string]domain.Cocktail)
+	// カクテルIDから材料構造体へのマップ
+	materialMap := make(map[string][]domain.Material)
 
 	for rows.Next() {
 		rows.Scan(
@@ -147,22 +158,16 @@ func (repo CocktailRepository) FindByMaterials(
 			&dbRecipe.amount,
 		)
 
-		if cocktailID == "" || dbCocktail.id == cocktailID {
-			material := domain.NewMaterial(dbMaterial.id, dbMaterial.name, dbMaterial.description, dbRecipe.amount)
-			materials = append(materials, material)
-		} else {
-			cocktail := domain.NewCocktail(dbCocktail.id, dbCocktail.name, dbCocktail.description, dbCocktailImage.image.String, materials)
-			cocktails = append(cocktails, cocktail)
+		// 材料ドメインのオブジェクトを生成
+		material := domain.NewMaterial(dbMaterial.id, dbMaterial.name, dbMaterial.description, dbRecipe.amount)
+		// マップへ紐づけ
+		materialMap[dbCocktail.id] = append(materialMap[dbCocktail.id], material)
 
-			cocktailID = dbCocktail.id
-			materials = nil
-		}
+		// カクテルドメインのオブジェクトを生成
+		cocktail := domain.NewCocktail(dbCocktail.id, dbCocktail.name, dbCocktail.description, dbCocktailImage.image.String, materialMap[dbCocktail.id])
+		// マップへ紐づけ
+		cocktailMap[dbCocktail.id] = cocktail
 	}
 
-	// TODO: 確認用、後で消す
-	for _, contents := range cocktails {
-		fmt.Println(contents)
-	}
-
-	return cocktails, nil
+	return cocktailMap, nil
 }
