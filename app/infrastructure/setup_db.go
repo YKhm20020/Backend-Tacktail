d7199cbb60540a91507dcbbc08ef49724ed5e837
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-15 08:26:42 +0900

    update: Cloud SQL との接続を確立するため、DBとの接続方法を変更

--- app/infrastructure/setup_db.go
+++ app/infrastructure/setup_db.go
@@ -9,11 +9,12 @@ import (
 )
 
 var (
-	host     string
-	user     string
-	password string
-	dbname   string
-	port     string
+	host           string
+	user           string
+	password       string
+	dbname         string
+	port           string
+	unixSocketPath string
 )
 
 func SetupDB() *sql.DB {
@@ -22,10 +23,13 @@ func SetupDB() *sql.DB {
 		panic("完了変数の取得に失敗")
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		host, user, password, dbname, port)
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	// 	host, user, password, dbname, port)
 
-	db, err := sql.Open("postgres", dsn)
+	dbURI := fmt.Sprintf("%s:%s@unix(%s)/%s?parseTime=true",
+		user, password, unixSocketPath, dbname)
+
+	db, err := sql.Open("postgres", dbURI)
 	if err != nil {
 		panic("データベース接続失敗")
 	} else {
@@ -36,9 +40,11 @@ func SetupDB() *sql.DB {
 }
 
 func loadEnv() error {
-	err := godotenv.Load(".env")
-	if err != nil {
-		return err
+	if product := os.Getenv(("PRODUCTION")); product == "" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			fmt.Println("fail to load .env file")
+		}
 	}
 
 	host = os.Getenv("DB_HOST")
@@ -46,6 +52,7 @@ func loadEnv() error {
 	password = os.Getenv("DB_PASSWORD")
 	dbname = os.Getenv("DB_NAME")
 	port = os.Getenv("DB_PORT")
+	unixSocketPath = os.Getenv("INSTANCE_UNIX_SOCKET")
 
 	return nil
 }
