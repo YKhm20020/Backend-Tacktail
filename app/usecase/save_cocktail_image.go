c48fd54aa5815f754d7fed1346dcf48c53a98351
KakinokiKanta <kanta12621@gmail.com>
2024-11-14 15:05:14 +0900

    add: カクテル画像のパスを保存するユースケースのDTOを定義

--- app/usecase/save_cocktail_image.go
+++ app/usecase/save_cocktail_image.go
@@ -6,8 +6,23 @@ type SaveCocktailImage struct {
 	repo domain.CocktailRepository
 }
 
+type SaveCocktailImageInput struct {
+	CocktailID string `json:"cocktail_id" binding:"required"`
+	UserID     string `json:"user_id" binding:"required"`
+	Image      string `json:"image" binding:"required"`
+}
+
+type SaveCocktailImageOutput struct {
+	ImageID    string `json:"image_id"`
+	CocktailID string `json:"cocktail_id"`
+	UserID     string `json:"user_id"`
+	Image      string `json:"image"`
+}
+
 func NewSaveCocktailImage(repo domain.CocktailRepository) SaveCocktailImage {
 	return SaveCocktailImage{
 		repo: repo,
 	}
 }
+
+// func (uc SaveCocktailImage) Execute()
