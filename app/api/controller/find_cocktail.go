30c03b22ac0ebb0877a6c3d417c7fe29750d20ea
KakinokiKanta <kanta12621@gmail.com>
2024-11-14 21:59:09 +0900

    add: GinコンテキストからユーザーIDを取得し、有無に応じた処理を追加

--- app/api/controller/find_cocktail.go
+++ app/api/controller/find_cocktail.go
@@ -23,6 +23,14 @@ func (con FindCocktail) Execute(ctx *gin.Context) {
 	// /cocktails/{id} からカクテルIDを取得する
 	input.ID = ctx.Param("id")
 
+	// ヘッダー内のtokenからユーザーIDを取得
+	user_id, exists := ctx.Get("user_id")
+	if exists {
+		input.UserID = user_id.(string)
+	} else {
+		input.UserID = ""
+	}
+
 	output, err := con.uc.Execute(input)
 	if err != nil {
 		ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
