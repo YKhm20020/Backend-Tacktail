762f9ec3b2f9d3e0bd8aa6aefff09f3699ac28c6
KakinokiKanta <kanta12621@gmail.com>
2024-11-14 21:39:35 +0900

    add: ID指定でカクテル情報を取得するコントローラーを実装

--- app/api/controller/find_cocktail.go
+++ app/api/controller/find_cocktail.go
@@ -1,6 +1,11 @@
 package controller
 
-import "github.com/YKhm20020/Backend-Tacktail/usecase"
+import (
+	"net/http"
+
+	"github.com/YKhm20020/Backend-Tacktail/usecase"
+	"github.com/gin-gonic/gin"
+)
 
 type FindCocktail struct {
 	uc usecase.FindCocktail
@@ -10,4 +15,19 @@ func NewFindCocktail(uc usecase.FindCocktail) FindCocktail {
 	return FindCocktail{
 		uc: uc,
 	}
+}
+
+func (con FindCocktail) Execute(ctx *gin.Context) {
+	var input usecase.FindCocktailInput
+
+	// /cocktails/{id} からカクテルIDを取得する
+	input.ID = ctx.Param("id")
+
+	output, err := con.uc.Execute(input)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, output)
 }
