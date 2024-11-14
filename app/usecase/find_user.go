4513f0c7b78204d2cf1489867c896d9eb4cdee34
KakinokiKanta <kanta12621@gmail.com>
2024-11-15 00:29:22 +0900

    add: ユーザー情報を取得するユースケースのDTOを定義

--- app/usecase/find_user.go
+++ app/usecase/find_user.go
@@ -8,6 +8,17 @@ type FindUser struct {
 	repo domain.UserRepository
 }
 
+type FindUserInput struct {
+	ID string
+}
+
+type FindUserOutput struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+	Story    int    `json:"story"`
+}
+
 func NewFindUser(
 	repo domain.UserRepository,
 ) FindUser {
