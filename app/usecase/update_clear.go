ab8d139a47ce954e72b56cf6b39780efd78f15ee
KakinokiKanta <kanta12621@gmail.com>
2024-11-15 01:00:47 +0900

    add: 攻略個数を更新するユースケース層を実装

--- app/usecase/update_clear.go
+++ app/usecase/update_clear.go
@@ -5,7 +5,7 @@ import (
 )
 
 type UpdateClear struct {
-	repo domain.CocktailRepository
+	repo domain.UserRepository
 }
 
 type UpdateClearInput struct {
@@ -17,8 +17,25 @@ type UpdateClearOutput struct {
 	Story  int    `json:"story"`
 }
 
-func NewUpdateClear(repo domain.CocktailRepository) UpdateClear {
+func NewUpdateClear(repo domain.UserRepository) UpdateClear {
 	return UpdateClear{
 		repo: repo,
 	}
+}
+
+func (uc UpdateClear) Execute(input UpdateClearInput) (UpdateClearOutput, error) {
+	err := uc.repo.UpdateStory(input.UserID)
+	if err != nil {
+		return UpdateClearOutput{}, err
+	}
+
+	user, err := uc.repo.FindByID(input.UserID)
+	if err != nil {
+		return UpdateClearOutput{}, err
+	}
+
+	return UpdateClearOutput{
+		user.ID(),
+		user.Story(),
+	}, nil
 }
