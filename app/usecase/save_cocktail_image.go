9b2d053d58adae0430290eb793f44ce30dae1330
KakinokiKanta <kanta12621@gmail.com>
2024-11-14 16:27:03 +0900

    add: カクテル画像のパスを保存するユースケースを実装

--- app/usecase/save_cocktail_image.go
+++ app/usecase/save_cocktail_image.go
@@ -1,21 +1,24 @@
 package usecase
 
-import "github.com/YKhm20020/Backend-Tacktail/domain"
+import (
+	"github.com/YKhm20020/Backend-Tacktail/domain"
+	"github.com/oklog/ulid/v2"
+)
 
 type SaveCocktailImage struct {
 	repo domain.CocktailRepository
 }
 
 type SaveCocktailImageInput struct {
-	CocktailID string `json:"cocktail_id" binding:"required"`
 	UserID     string `json:"user_id" binding:"required"`
+	CocktailID string `json:"cocktail_id" binding:"required"`
 	Image      string `json:"image" binding:"required"`
 }
 
 type SaveCocktailImageOutput struct {
 	ImageID    string `json:"image_id"`
-	CocktailID string `json:"cocktail_id"`
 	UserID     string `json:"user_id"`
+	CocktailID string `json:"cocktail_id"`
 	Image      string `json:"image"`
 }
 
@@ -25,4 +28,36 @@ func NewSaveCocktailImage(repo domain.CocktailRepository) SaveCocktailImage {
 	}
 }
 
-// func (uc SaveCocktailImage) Execute()
+func (uc SaveCocktailImage) Execute(input SaveCocktailImageInput) (SaveCocktailImageOutput, error) {
+	// カクテルIDとユーザーIDから画像パスがDB内に存在するかチェック
+	cocktailImageID, err := uc.repo.FindImage(input.UserID, input.CocktailID)
+	if err == nil {
+		_, err = uc.repo.UpdateCocktailImage(cocktailImageID, input.Image)
+		if err != nil {
+			return SaveCocktailImageOutput{}, err
+		}
+
+		return SaveCocktailImageOutput{
+			cocktailImageID,
+			input.UserID,
+			input.CocktailID,
+			input.Image,
+		}, nil
+	}
+
+	if err.Error() != "not found image" {
+		return SaveCocktailImageOutput{}, err
+	}
+
+	cocktailImageID, err = uc.repo.InsertCocktailImage(ulid.Make().String(), input.Image)
+	if err != nil {
+		return SaveCocktailImageOutput{}, err
+	}
+
+	return SaveCocktailImageOutput{
+		cocktailImageID,
+		input.UserID,
+		input.CocktailID,
+		input.Image,
+	}, nil
+}
