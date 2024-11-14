c613b170bfefa46469b8527a1bf2eb739a86d0eb
KakinokiKanta <kanta12621@gmail.com>
2024-11-14 16:53:35 +0900

    add: カクテル画像のパスを保存するユースケースを実装

--- app/domain/cocktail.go
+++ app/domain/cocktail.go
@@ -10,7 +10,7 @@ type Cocktail struct {
 
 type CocktailRepository interface {
 	InsertCocktailImage(string, string, string, string) (string, error) // 引数はカクテル画像ID、ユーザーID、カクテルID、画像パスで、カクテル画像IDを取得
-	UpdateCocktailImage(string, string, string, string) (string, error) // 引数はカクテル画像ID、ユーザーID、カクテルID、画像パスで、カクテル画像IDを取得
+	UpdateCocktailImage(string, string) (string, error)                 // 引数はカクテル画像ID、画像パスで、カクテル画像IDを取得
 	FindAll(string) (map[string]Cocktail, error)                        // 引数はユーザーIDで、すべてのカクテルを取得
 	FindByMaterials(string, []string) (map[string]Cocktail, error)      // 引数はユーザーIDと材料IDのリスト
 	FindImage(string, string) (string, error)                           // 引数はユーザーID、カクテルIDで、カクテル画像IDを取得
