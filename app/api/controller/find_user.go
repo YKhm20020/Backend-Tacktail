9f9d102516e118cf30020aa5cb4e3dfdee98116b
KakinokiKanta <kanta12621@gmail.com>
2024-11-15 00:44:36 +0900

    add: ユーザー情報を取得するコントローラーを実装

--- app/api/controller/find_user.go
+++ app/api/controller/find_user.go
@@ -1,7 +1,11 @@
 package controller
 
 import (
+	"errors"
+	"net/http"
+
 	"github.com/YKhm20020/Backend-Tacktail/usecase"
+	"github.com/gin-gonic/gin"
 )
 
 type FindUser struct {
@@ -12,4 +16,24 @@ func NewFindUser(uc usecase.FindUser) FindUser {
 	return FindUser{
 		uc: uc,
 	}
+}
+
+func (con FindUser) Execute(ctx *gin.Context) {
+	var input usecase.FindUserInput
+
+	// ヘッダー内のtokenからユーザーIDを取得
+	user_id, exists := ctx.Get("user_id")
+	if !exists {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": errors.New("ヘッダー内のトークンからユーザーIDを取得できませんでした")})
+		return
+	}
+
+	input.ID = user_id.(string)
+	output, err := con.uc.Execute(input)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, output)
 }
