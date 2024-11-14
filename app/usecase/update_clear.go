2a5030e3af9e20c3b0f3f3c685be91bdf38baec0
KakinokiKanta <kanta12621@gmail.com>
2024-11-15 00:53:27 +0900

    add: 攻略個数を更新するユースケース層のDTOを定義

--- app/usecase/update_clear.go
+++ app/usecase/update_clear.go
@@ -8,6 +8,15 @@ type UpdateClear struct {
 	repo domain.CocktailRepository
 }
 
+type UpdateClearInput struct {
+	UserID string
+}
+
+type UpdateClearOutput struct {
+	UserID string `json:"user_id"`
+	Story  int    `json:"story"`
+}
+
 func NewUpdateClear(repo domain.CocktailRepository) UpdateClear {
 	return UpdateClear{
 		repo: repo,
