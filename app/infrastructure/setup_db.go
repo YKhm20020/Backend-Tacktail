925212c9a62e43b0d64e07c8f042185c6322299e
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-15 23:08:24 +0900

    update: 開発環境用とデプロイ環境用でDBへの接続方法を分けて実装

--- app/infrastructure/setup_db.go
+++ app/infrastructure/setup_db.go
@@ -1,50 +1,69 @@
 package infrastructure
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"log"
+	"net"
 	"os"
+
+	"cloud.google.com/go/cloudsqlconn"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 
 	"github.com/joho/godotenv"
 )
 
 var (
-	host           string
-	user           string
-	password       string
-	dbname         string
-	port           string
-	unixSocketPath string
+	host     string
+	user     string
+	password string
+	dbname   string
+	port     string
 )
 
 func SetupDB() *sql.DB {
-	err := loadEnv()
-	if err != nil {
-		panic("完了変数の取得に失敗")
-	}
-
-	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-	// 	host, user, password, dbname, port)
-
-	dbURI := fmt.Sprintf("%s:%s@unix(%s)/%s?parseTime=true",
-		user, password, unixSocketPath, dbname)
-
-	db, err := sql.Open("postgres", dbURI)
-	if err != nil {
-		panic("データベース接続失敗")
-	} else {
-		fmt.Println("データベース接続成功")
-	}
-
-	return db
-}
-
-func loadEnv() error {
 	if product := os.Getenv(("PRODUCTION")); product == "" {
-		err := godotenv.Load(".env")
+		// 開発環境
+
+		err := loadEnv()
+		if err != nil {
+			panic("開発環境での完了変数の取得に失敗")
+		}
+
 		if err != nil {
 			fmt.Println("fail to load .env file")
 		}
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			host, user, password, dbname, port)
+
+		db, err := sql.Open("postgres", dsn)
+		if err != nil {
+			panic("データベース接続失敗")
+		} else {
+			fmt.Println("データベース接続成功")
+		}
+
+		return db
+	} else {
+		// デプロイ環境では Google Cloud に接続
+		db, err := connectWithConnector()
+
+		if err != nil {
+			panic("デプロイ環境でのDB接続失敗")
+		}
+
+		fmt.Println("デプロイ環境でデータベース接続成功")
+		return db
+	}
+
+}
+
+func loadEnv() error {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return err
 	}
 
 	host = os.Getenv("DB_HOST")
@@ -52,7 +71,52 @@ func loadEnv() error {
 	password = os.Getenv("DB_PASSWORD")
 	dbname = os.Getenv("DB_NAME")
 	port = os.Getenv("DB_PORT")
-	unixSocketPath = os.Getenv("INSTANCE_UNIX_SOCKET")
 
 	return nil
+}
+
+func connectWithConnector() (*sql.DB, error) {
+	mustGetenv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("Fatal Error in connect_connector.go: %s environment variable not set.\n", k)
+		}
+		return v
+	}
+	// Note: Saving credentials in environment variables is convenient, but not
+	// secure - consider a more secure solution such as
+	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+	// keep passwords and other secrets safe.
+	var (
+		dbUser                 = mustGetenv("DB_USER")                  // e.g. 'my-db-user'
+		dbPwd                  = mustGetenv("DB_PASS")                  // e.g. 'my-db-password'
+		dbName                 = mustGetenv("DB_NAME")                  // e.g. 'my-database'
+		instanceConnectionName = mustGetenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
+		usePrivate             = os.Getenv("PRIVATE_IP")
+	)
+
+	dsn := fmt.Sprintf("user=%s password=%s database=%s", dbUser, dbPwd, dbName)
+	config, err := pgx.ParseConfig(dsn)
+	if err != nil {
+		return nil, err
+	}
+	var opts []cloudsqlconn.Option
+	if usePrivate != "" {
+		opts = append(opts, cloudsqlconn.WithDefaultDialOptions(cloudsqlconn.WithPrivateIP()))
+	}
+	d, err := cloudsqlconn.NewDialer(context.Background(), opts...)
+	if err != nil {
+		return nil, err
+	}
+	// Use the Cloud SQL connector to handle connecting to the instance.
+	// This approach does *NOT* require the Cloud SQL proxy.
+	config.DialFunc = func(ctx context.Context, network, instance string) (net.Conn, error) {
+		return d.Dial(ctx, instanceConnectionName)
+	}
+	dbURI := stdlib.RegisterConnConfig(config)
+	dbPool, err := sql.Open("pgx", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %w", err)
+	}
+	return dbPool, nil
 }
